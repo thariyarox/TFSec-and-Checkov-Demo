e4ea7caf98f0a229e9a00ff2c4f283095888523d
Tharindu Edirisinghe <thariyarox@gmail.com>
2022-09-30 13:57:53 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "bad_example" {
+  ami = "ami-7f89a64f"
+  instance_type = "t1.micro"
+
+  root_block_device {
+      encrypted = false
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = false
+    encrypted = false
+  }
+}
